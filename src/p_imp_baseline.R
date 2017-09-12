39671f45afe2652d6480217815d64d52af326ffb
Connor Gilroy <ccgilroy@gmail.com>
2017-09-12 13:25:18 -0700

    add better plot that visualizes scores + variables with points, not bars, relative to baseline

--- src/p_imp_baseline.R
+++ src/p_imp_baseline.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(forcats)
 library(viridis)
 
+options(scipen = 999)
+
 scores <- read_csv(file.path("data", "ffc_models_leaderboard_scores.csv"))
 
 baseline <- 
@@ -15,7 +17,16 @@ scores <-
   filter(model != "baseline") %>%
   mutate(data = as_factor(data)) %>%
   select(-`FF submission no`, -`name of submission zip`) %>%
-  select(name = `model name`, everything())
+  mutate(
+    vars_and_scores = case_when(
+      scores == "none" ~ variables,
+      scores == "expert" ~ "hp + expert scores",
+      scores == "mturk" ~ "hp + mturk scores"
+    ), 
+    vars_and_scores = as_factor(vars_and_scores)
+  ) %>%
+  select(name = `model name`, model, scores, variables, vars_and_scores,
+         everything()) 
   
 scores_long <-
   scores %>%
@@ -102,3 +113,22 @@ scores_long %>%
        title = "Comparison of Mean Squared Error by Imputation Strategy", 
        subtitle = "For all sets of variables with no ranking information")
 
+p_imp_baseline <- 
+  scores_long %>%
+  ggplot(aes(x = fct_rev(vars_and_scores), y = rel_mse, color = fct_rev(data))) + 
+  facet_wrap(~ outcome, ncol = 2, scales = "free_x", dir = "v") +
+  geom_hline(yintercept = 0) +
+  scale_color_viridis(discrete = TRUE) +
+  geom_point() + 
+  coord_flip() + 
+  theme_minimal() + 
+  guides(color = guide_legend(reverse = TRUE)) +
+  labs(color = NULL, 
+       x = NULL, 
+       y = "Out-of-sample MSE - baseline, from leaderboard data", 
+       title = "Mean Squared Error relative to Baseline") + 
+  theme(panel.spacing.x = unit(3, "lines"))
+
+ggsave(filename = file.path("output", "p_imp_baseline.png"), 
+       plot = p_imp_baseline, 
+       height = 5, width = 8)
