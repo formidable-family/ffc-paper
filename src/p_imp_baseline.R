9cc4d08330865332e0343fec928a8462bcbd2214
Connor Gilroy <ccgilroy@gmail.com>
2017-09-14 11:50:04 -0700

    reverse plotting of data and variables in new plot

--- src/p_imp_baseline.R
+++ src/p_imp_baseline.R
@@ -132,3 +132,24 @@ p_imp_baseline <-
 ggsave(filename = file.path("output", "p_imp_baseline.png"), 
        plot = p_imp_baseline, 
        height = 5, width = 8)
+
+p_imp_baseline_2 <- 
+  scores_long %>%
+  mutate(data = fct_relevel(data, "mean imputation", "untyped OLS regression imputation")) %>%
+  ggplot(aes(x = fct_rev(data), y = rel_mse, color = fct_rev(vars_and_scores))) + 
+  facet_wrap(~ outcome, ncol = 2, scales = "free_x", dir = "v") +
+  geom_hline(yintercept = 0) +
+  scale_color_viridis(discrete = TRUE) +
+  geom_point() + 
+  coord_flip() + 
+  theme_minimal() + 
+  guides(color = guide_legend(reverse = TRUE)) +
+  labs(color = "Variables and priors", 
+       x = NULL, 
+       y = "Out-of-sample MSE - baseline, from leaderboard data", 
+       title = "Mean Squared Error relative to Baseline") + 
+  theme(panel.spacing.x = unit(3, "lines"))
+
+ggsave(filename = file.path("output", "p_imp_baseline_2.png"), 
+       plot = p_imp_baseline_2, 
+       height = 5, width = 8)
